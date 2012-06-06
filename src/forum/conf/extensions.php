e3d2af74b8c8bad43928f88acdf6f3301ca919a1
Tristan Rivoallan <tristan@rivoallan.net>
2012-06-06 17:27:55 +0200

    [CiTwigTemplates] created and activated stub extension

--- src/forum/conf/extensions.php
+++ src/forum/conf/extensions.php
@@ -1,9 +1,13 @@
 <?php
 // Make sure this file was not accessed directly and prevent register_globals configuration array attack
-if (!defined('IN_VANILLA')) exit();
-// Enabled Extensions
+if (!defined('IN_VANILLA')) {
+    exit();
+}
+
+// Native extensions
 include($Configuration['EXTENSIONS_PATH']."DiscussionPages/default.php");
 include($Configuration['EXTENSIONS_PATH']."NewApplicants/default.php");
-include($Configuration['EXTENSIONS_PATH']."PreviewPost/default.php");
 include($Configuration['EXTENSIONS_PATH']."Whisperfi/default.php");
-?>
+
+// Custom extensions
+include($Configuration['EXTENSIONS_PATH']."CiTwigTemplates/default.php");
---
+++ src/forum/extensions/CiTwigTemplates/default.php
@@ -0,0 +1,8 @@
+<?php
+/**
+Extension Name: CiTwigTemplates
+Extension Url: https://github.com/contructions-incongrues/net.musiques-incongrues.next/tree/master/src/forum/extensions/CiTwigTemplates
+Description: Makes it possible to use the Twig (http://twig.sensiolabs.org/) templating engine
+Author: Constructions Incongrues
+Author Url: http://www.constructions-incongrues.net
+*/
